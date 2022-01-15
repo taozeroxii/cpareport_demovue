8f7259df3c284df01bf7e6f5e372b7d5bffed354
unknown <tao32040@gmail.com>
2022-01-15 15:35:48 +0700

    ทดสอบการใช้งาน watch ของ vuejs

--- src/views/admin/Addquery.vue
+++ src/views/admin/Addquery.vue
@@ -1,6 +1,63 @@
 <template>
   <v-container>
+
     <MenuTabadmin />
+
+    <v-row class="justify-center mt-5">
+      <v-card style="width:90%;" outline>
+        <v-img class="align-end" height="100px">
+          <v-card-title primary-title>
+            ADD QUERYs
+          </v-card-title>
+        </v-img>
+        <v-card-text>
+          SQL file : 413
+          <v-form @submit.prevent="submit">
+            <v-row>
+              <v-col class="d-flex" lg="2" cols="4">
+                <v-select
+                  v-validate="'required'"
+                  name="main_menu"
+                  v-model="main_menu"
+                  :items="items"
+                  label="main_menu"
+                  :rules="main_menuRule"
+                  :class="{ 'v-input--has-state theme--light v-text-field v-text-field--is-booted error--text': errors.has( 'main_menuRule' ), }"
+                ></v-select>
+              </v-col>
+
+              <v-col lg="1" cols="1">
+                <v-text-field   name="menu_maxid" label="เมนูที่"    v-model="menu_maxid"  disabled/>
+                <!-- <span>{{ errors.first("lname") }}</span> -->
+              </v-col>
+            </v-row>
+
+            <v-row class="justify-space-between px-3 pt-5 mb-5">
+              <v-col cols="6">
+                <v-btn type="submit" block color="success">
+                  บันทึก<v-icon dark right> mdi-checkbox-marked-circle </v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="6">
+                <v-btn block color="orange darken-2" dark @click="cancle"><v-icon dark left> mdi-arrow-left</v-icon>ย้อนกลับ</v-btn>
+              </v-col>
+
+              <v-col>
+                <v-alert
+                  v-if="errorMessage"
+                  outlined
+                  type="warning"
+                  prominent
+                  border="left"
+                >
+                  {{ errorMessage }}</v-alert
+                >
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
@@ -13,9 +70,35 @@ export default {
   },
   data() {
     return {
-      name: null,
+      items     : ["1", "2", "3"],
+      //ตัวแปรไว้รับค่าจากฟอร์มเพื่อส่งไปบันทึกลงฐานข้อมูล
+      form:{},
+      main_menu : "",
+      menu_maxid:"",
+
+      //error handling เช็ค error ก่อนบันทึกข้อมูล
+      errorMessage: "",
+      main_menuRule: [(v1) => !!v1 || "โปรดกรอก เลือกหัวข้อหลัก"],
     };
   },
+  methods: {
+    cancle() {
+      this.$router.back();
+    },
+    getMaxmenu_id(selected_id){
+      this.menu_maxid = selected_id+1;
+    }
+  },
+
+  watch: {
+    // whenever main_menu changes, this function will run
+    main_menu: function (val) {// หากเลือกเมนูแล้วให้ส่งค่า value ขอเมนูนั้นๆมาเพเื่อเรียกใช้ฟังก์ชันต่อ
+      // console.log(val)
+      this.getMaxmenu_id(val)
+    },
+
+  },
+
 };
 </script>
 
