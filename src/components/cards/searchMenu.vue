c50c88818ed4ead68e198edbf38cbf639bc06bb2
unknown <tao32040@gmail.com>
2022-07-11 16:30:14 +0700

    Update searchMenu.vue
    - เพิ่ม เก็บ log หาก search memu ด้านบน (เดิมไม่ได้เก็บไว้เก็บแค่กดเรียกเมนูตามปกติ)

--- src/components/cards/searchMenu.vue
+++ src/components/cards/searchMenu.vue
@@ -24,28 +24,68 @@
 </template>
 
 <script>
+import axios from "axios";
+import moment from "moment";
 export default {
   name: "searchMenu",
   props: ["searchmenu"],
   data() {
     return {
       menumain: null,
+       detailuser: {
+        session: null,
+        ip: null,
+        username: null,
+        sql_file: null,
+        datetime: null,
+      },
     };
   },
-  methods: {
-    onClickMenu(menu_file,menu_link) {
-      if(menu_file == null || menu_file == '' || !menu_file.match(/sql_.*/) ){
-        // console.log(`http://172.16.0.251/report/${menu_link}`)
-        if(menu_link.match(/report.*/)){
-           window.open ("http://172.16.0.251/"+menu_link,"_blank");
-        }else if(menu_link.match(/https.*/)){
-          window.open (menu_link,"_blank");
-        } 
-      }else {
-        this.$router.push(`./tableshowdata/${menu_file}`).catch(() => {});
+  created() {
+    axios.get("http://172.16.0.251:3010/api/menu/getclientip", {headers: { "x-access-token": localStorage.token },}).then((response) => {
+      this.detailuser.ip = response.data;
+      if (
+        typeof localStorage.username == "undefined" ||
+        localStorage.username == ""
+      ) {
+        this.detailuser.username = "guest";
+        this.detailuser.session = "guest";
+      } else {
+        this.detailuser.username = localStorage.username;
+        this.detailuser.session = localStorage.token;
       }
-    
+
+      this.detailuser.datetime = moment(new Date()).format(
+        "YYYY-MM-DD HH:mm:ss"
+      );
+      localStorage.ip = response.data;
+      console.log("Client Ip : " + response.data);
+    });
+  },
+
+
+
+  methods: {
+
+    onClickMenu(menu_file,menu_link) {
+      this.detailuser.sql_file = menu_file;
+      // console.log(menu_link)
+      axios.post( `http://172.16.0.251:3010/api/menu/stampviewer`, this.detailuser).then(() => {
+        if(menu_file == null || menu_file == '' || !menu_file.match(/sql_.*/) ){
+          // console.log(`http://172.16.0.251/report/${menu_link}`)
+          if(menu_link.match(/report.*/)){
+            window.open ("http://172.16.0.251/"+menu_link,"_blank");
+          }else if(menu_link.match(/https.*/)){
+            window.open (menu_link,"_blank");
+          } 
+        }else {
+          this.$router.push(`./tableshowdata/${menu_file}`).catch(() => {});
+        }
+      });
     },
+
+
+
   },
 };
 </script>
